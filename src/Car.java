ef325f14cb6a49001d1f25b0fff7147b0f61a73a
pomjoep <59618718+pomjoep@users.noreply.github.com>
2024-04-02 17:20:48 -0700

    started

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+public class Car{
+    int odometer = 0;
+    String make = "make";
+    String model = "model";
+
+    public static void main(String[]args){
+        Car c1 = new Car();
+
+    }
+}
