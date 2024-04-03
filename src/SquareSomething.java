1248a93cec6d605c55aa5d4360e7efb37b9bb22e
pomjoep <59618718+pomjoep@users.noreply.github.com>
2024-04-02 23:19:47 -0700

    square done

---
+++ src/SquareSomething.java
@@ -0,0 +1,11 @@
+public class SquareSomething{
+    public static int square(int number){
+        return number *= number;
+    }
+    public static double square(double dub){
+        return dub *= dub;
+    }
+    public static float square(float num){
+        return num *= num;
+    }
+}
