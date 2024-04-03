e06af6148b587ac5aa9d4e1b74cc351d196ba1f6
pomjoep <59618718+pomjoep@users.noreply.github.com>
2024-04-02 22:35:00 -0700

    car done

--- src/Car.java
+++ src/Car.java
@@ -5,6 +5,27 @@ public class Car{
 
     public static void main(String[]args){
         Car c1 = new Car();
-
+        System.out.println(c1.toString());
     }
+
+    //constructors
+    public Car() {
+    }
+    public Car(String make){
+        this.make = make;
+    }
+    public Car(String make, String model){
+        this.make = make;
+        this.model = model;
+    }
+
+    @Override
+    public String toString(){
+        return STR."Make: \{make}, Model: \{model}, Miles: \{odometer}";
+    }
+
+    public Boolean equals(Car anotherCar){
+        return odometer == anotherCar.odometer && make.equalsIgnoreCase(anotherCar.make) && model.equalsIgnoreCase(anotherCar.model);
+    }
+
 }
